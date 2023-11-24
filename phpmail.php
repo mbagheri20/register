90d13ab3caf743ce1ec3ce951b3efa630f11f405
Mohammad Bagheri <74108802+mbagheri20@users.noreply.github.com>
2023-11-24 12:47:26 +0200

    Update phpmail.php

--- phpmail.php
+++ phpmail.php
@@ -1,6 +1,8 @@
 <?php 
-
-$email = "mohammad.bagheri.fi@gmail.com";
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+$email = "mohammad.bagheri@oulu.fi";
 $subject =  "Email Test";
 $message = "this is a mail testing email function on server";
 
