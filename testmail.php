c1e9e7d63e6f521dcd91980f2016557b4c115928
Mohammad <mohammad.bagheri@oulu.fi>
2023-11-25 15:29:45 +0200

    add PHPMailer

--- testmail.php
+++ testmail.php
@@ -13,7 +13,7 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.pouta.csc.fi';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.pouta.csc.fi:25';                     //Set the SMTP server to send through
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 25;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
