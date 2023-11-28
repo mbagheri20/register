a0ed39c6ec1b051aa04f6fae7f8c881f35200b94
Mohammad Bagheri <74108802+mbagheri20@users.noreply.github.com>
2023-11-28 13:09:44 +0200

    Update register.php

--- register.php
+++ register.php
@@ -427,7 +427,7 @@ if($try_send){
   }
 
   //$sent_successfully = mail($regmail_to, $regmail_title, $regmail_contents,$regmail_headers );
-  $mail->Body    = nl2br($regmail_contents);
+  $mail->Body    = $regmail_contents;
   $sent_successfully = $mail->send();
 	
 
