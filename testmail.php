d5006d703161120800dfcb4db2595d65e96cad29
Mohammad Bagheri <74108802+mbagheri20@users.noreply.github.com>
2024-02-08 20:34:33 +0200

    Update testmail.php

--- testmail.php
+++ testmail.php
@@ -45,7 +45,7 @@ try {
     //$mail->Body =  html_entity_decode($Body);
     $mail->Subject = 'first project report Nov';
     $mail->Body    = 'Hi, If you received this email, it means the PHP mail fixed!';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     $mail->isHTML(true);
     $mail->send();
     echo 'Message has been sent';
