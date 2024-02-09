e3c6496b3db7e0e365b5ecaa34e23f19e26fc128
Mohammad Bagheri <74108802+mbagheri20@users.noreply.github.com>
2024-02-09 06:09:51 +0200

    Update testmail.php

--- testmail.php
+++ testmail.php
@@ -44,7 +44,7 @@ try {
                                       //Set email format to HTML
     //$mail->Body =  html_entity_decode($Body);
     $mail->Subject = 'first project report Nov';
-    $mail->Body    = 'Hi, If you received this email, it means the PHP mail fixed!';
+    $mail->Body    = ' <p><b>HTML Mail</b> Test</p> Hi, If you received this email, it means the PHP mail fixed!';
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     $mail->isHTML(true);
     $mail->send();
